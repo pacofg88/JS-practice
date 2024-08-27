f3d6d26880fded14ebabcc36080f0ef816d56a06
pacofg <pacofg2010@gmail.com>
2024-08-27 23:18:54 +0900

    Modal window exercise added

---
+++ 05-Modal/script.js
@@ -0,0 +1,29 @@
+"use strict";
+
+// element dialog box
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+// button to close dialog box
+const btnCloseModal = document.querySelector(".close-modal");
+// object containing all the elements with the class
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+
+const openModal = function () {
+  console.log("Button clicked");
+  // the classList removes the "hidden" class
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  // the classList adds the "hidden" class
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener("click", openModal);
+
+btnCloseModal.addEventListener("click", closeModal);
+
+overlay.addEventListener("click", closeModal);
