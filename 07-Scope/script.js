b8eba35fd47132c3567fa98a66a52149360da688
pacofg <pacofg2010@gmail.com>
2024-09-03 00:15:37 +0900

    Practicing Scope/Scope Chain

---
+++ 07-Scope/script.js
@@ -0,0 +1,32 @@
+'use strict';
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    const output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      const firstName = 'Steven';
+
+      // Reassigning outer scope's variable
+      output = 'NEW OUTPUT';
+
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
+    // var don't care about blocks
+    console.log(millenial);
+  }
+  printAge();
+  return age;
+}
+
+const firstName = 'Francisco';
+calcAge(1992);
